261c1b3446c7192d7932d83d426e74884c0954e3
magr0s <magr0s.ak@gmail.com>
2018-05-02 21:28:26 +0300

    new lexicon

---
+++ core/components/modxsite/lexicon/cn/contship.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$_lang['contship.copy'] = '版權所有。';
+$_lang['contship.policy'] = '隱私政策';
+$_lang['contship.callnow'] = '聯繫我們';
+$_lang['contship.feedback'] = '聯繫我們';
+$_lang['contship.countries'] = '國家';
+$_lang['contship.featured_title'] = '我們的優勢';
---
+++ core/components/modxsite/lexicon/en/contship.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$_lang['contship.copy'] = 'All rights reserved.';
+$_lang['contship.policy'] = 'Privacy policy';
+$_lang['contship.callnow'] = 'Call us';
+$_lang['contship.feedback'] = 'Contact us';
+$_lang['contship.countries'] = 'Countries';
+$_lang['contship.featured_title'] = 'Our advantages';
